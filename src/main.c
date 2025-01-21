31ed77455d681fcbe404ff6045922f0d95600595
artinel <artinel@proton.me>
2025-01-21 12:02:06 +0330

    Initial code

---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include<std/io.h>
+#include"version.h"
+
+int main(int argc, char** argv){
+	prints("Welcome to Ash version %s\n", ASH_VERSION);
+	return 0;
+}
---
+++ src/version.h
@@ -0,0 +1,1 @@
+#define ASH_VERSION "0.0.1"
