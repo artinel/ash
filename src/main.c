add5962ea3997f356ca0fb3cbc5b4fd89d87ac21
artinel <artinel@proton.me>
2025-01-27 07:56:33 +0330

    Add the ability to search for programs in /bin(/usr/bin)

--- src/main.c
+++ src/main.c
@@ -3,14 +3,19 @@
 #include<sys/time.h>
 #include<std/mem.h>
 #include<sys/exec.h>
+#include<io/file.h>
+#include<error.h>
 #include"version.h"
 #include"command.h"
 #include"prompt.h"
 
 void readline();
 void process_cmd();
+void find_cmd(const char* cmd);
+void exec_cmd(const char* cmd, char** args);
 
 int main(int argc, char** argv){
+	error_autolog(false);
 	if(argc > 1){
 		if(str_equ(argv[1], "-v"))
 			prints("Ash shell v%s\n", ASH_VERSION);
@@ -37,18 +42,26 @@ void process_cmd(char* cmd){
 	}else if(str_equ(cmd, "exit")){
 		cmd_exit(0);
 	}else{
-		pid_t pid = fork();
-		if(pid == 0){
-			char* envp[1];
-			envp[0] = 0;
-			int res = exec(cmd, (const char* const*)args, (const char* const*)envp);
-			terminate(res);
+		if(cmd[0] == '.' && cmd[1] == '/'){
+			if(fexists(&cmd[2])){
+				exec_cmd(&cmd[2], args);		
+			}else{
+				printse(T_RED"Command '%s' not found!!!\n"T_NORMAL, cmd);
+			}
 		}else{
-			siginfo_t info;
-			pid_t res = waitid(P_PID, pid, &info, WEXITED, NULL);
-			prints("\n Process returned %d\n", res);
+			if(fexists(cmd)){
+				exec_cmd(cmd, args);
+			}else{
+				char* buf = malloc(PROMPT_BUFFER_SIZE + 1);
+				sprints(buf, "/bin/%s", cmd);
+				if(fexists(buf)){
+					exec_cmd(buf, args);
+				}else{
+					printse(T_RED"Command '%s' not found!!!\n"T_NORMAL, buf);
+				}
+				free(buf);
+			}
 		}
-			//printse(T_RED"Command not found!!!\n"T_NORMAL);	
 	}
 
 	free(args);
@@ -60,4 +73,17 @@ void readline(){
 		get_prompt(cmd);
 		process_cmd(cmd);
 	}
+}
+
+void exec_cmd(const char* cmd, char** args){
+	pid_t pid = fork();
+		if(pid == 0){
+			char* envp[1];
+			envp[0] = 0;
+			int res = exec(cmd, (const char* const*)args, (const char* const*)envp);
+			terminate(res);
+		}else{
+			siginfo_t info;
+			waitid(P_PID, pid, &info, WEXITED, NULL);
+		}
 }
